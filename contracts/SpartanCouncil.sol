f29d465762bb8a9fa2091249d4509a48cfb9c0f7
AndyTCF <andy.c.f.98@gmail.com>
2020-11-13 16:00:51 +1100

    remove build

--- contracts/SpartanCouncil.sol
+++ contracts/SpartanCouncil.sol
@@ -1,12 +1,8 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.16;
 
-import "@openzeppelin/contracts/GSN/Context.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/introspection/ERC165.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "openzeppelin-solidity-2.3.0/contracts/introspection/ERC165.sol";
+import "openzeppelin-solidity-2.3.0/contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-solidity-2.3.0/contracts/utils/Address.sol";
 
 
 /**
@@ -14,17 +10,16 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
  * @dev see https://eips.ethereum.org/EIPS/eip-721
  */
 contract SpartanCouncil is IERC721, ERC165 {
-    using SafeMath for uint256;
     using Address for address;
-    using Strings for uint256;
+
+    event Mint(uint256 tokenId, address to);
+    event Burn(uint256 tokenId);
 
     mapping(address => uint256) private _holderTokens;
 
     mapping(uint256 => address) private _tokenOwners;
 
-    // Equals to `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
-    // which can be also obtained as `IERC721Receiver(0).onERC721Received.selector`
-    bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
+    uint256 public tokenCount;
 
     // Token name
     string private _name;
@@ -44,81 +39,84 @@ contract SpartanCouncil is IERC721, ERC165 {
         _superOwner = msg.sender;
     }
 
+    modifier isSuperOwner() {
+        require(msg.sender == _superOwner, "Sender is not the super owner");
+        _;
+    }
+
+    modifier isValidAddress(address to) {
+        require(to != address(0), "ERC721: transfer to the zero address");
+        _;
+    }
+
     /**
      * @dev See {IERC721-balanceOf}.
      */
-    function balanceOf(address owner) public override view returns (uint256) {
-        require(
-            owner != address(0),
-            "ERC721: balance query for the zero address"
-        );
-
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), "ERC721: balance query for the zero address");
         return _holderTokens[owner];
     }
 
     /**
      * @dev See {IERC721-ownerOf}.
      */
-    function ownerOf(uint256 tokenId) public override view returns (address) {
+    function ownerOf(uint256 tokenId) public view returns (address) {
         return _tokenOwners[tokenId];
     }
 
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public {}
 
     /**
      * @dev See {IERC721-safeTransferFrom}.
      */
-    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
-        safeTransferFrom(from, to, tokenId, "");
-    }
-
-    /**
-     * @dev See {IERC721-safeTransferFrom}.
-     */
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
-        _safeTransfer(from, to, tokenId, _data);
-    }
-
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public {}
 
     /**
      * @dev See {IERC721-transferFrom}.
      */
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
-        _transfer(from, to, tokenId);
-    }
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public {}
 
     /**
      * @dev See {IERC721-approve}.
      */
-    function approve(address to, uint256 tokenId) public virtual override {}
+    function approve(address to, uint256 tokenId) public {}
 
     /**
      * @dev See {IERC721-isApprovedForAll}.
      */
-    function isApprovedForAll(address owner, address operator) public view override returns (bool) {}
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {}
 
-/**
+    /**
      * @dev See {IERC721-getApproved}.
      */
-    function getApproved(uint256 tokenId) public view override returns (address) {}
+    function getApproved(uint256 tokenId) public view returns (address) {}
 
     /**
      * @dev See {IERC721-setApprovalForAll}.
      */
-    function setApprovalForAll(address operator, bool approved) public virtual override {}
+    function setApprovalForAll(address operator, bool approved) public {}
 
-
-
-
-    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory _data) internal virtual {
-        _transfer(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
-    }
-
-
-    function _transfer(address from, address to, uint256 tokenId) internal virtual {
-        require(msg.sender == _superOwner, "Sender is not the super owner");
-        require(to != address(0), "ERC721: transfer to the zero address");
-
+    function transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public isSuperOwner() isValidAddress(to) {
         delete _holderTokens[from];
         _holderTokens[to] = tokenId;
 
@@ -127,31 +125,33 @@ contract SpartanCouncil is IERC721, ERC165 {
         emit Transfer(from, to, tokenId);
     }
 
-    /**
-     * @dev Internal function to invoke {IERC721Receiver-onERC721Received} on a target address.
-     * The call is not executed if the target address is not a contract.
-     *
-     * @param from address representing the previous owner of the given token ID
-     * @param to target address that will receive the tokens
-     * @param tokenId uint256 ID of the token to be transferred
-     * @param _data bytes optional data to send along with the call
-     * @return bool whether the call correctly returned the expected magic value
-     */
-    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data)
-        private returns (bool)
-    {
-        if (!to.isContract()) {
-            return true;
-        }
-        bytes memory returndata = to.functionCall(abi.encodeWithSelector(
-            IERC721Receiver(to).onERC721Received.selector,
-            msg.sender,
-            from,
-            tokenId,
-            _data
-        ), "ERC721: transfer to non ERC721Receiver implementer");
-        bytes4 retval = abi.decode(returndata, (bytes4));
-        return (retval == _ERC721_RECEIVED);
+    function mint(address to, uint256 tokenId) public isSuperOwner() isValidAddress(to) {
+        require(tokenId != tokenCount, "ERC721: token already minted");
+
+        _holderTokens[to] = tokenId;
+
+        tokenCount += 1;
+
+        _tokenOwners[tokenId] = to;
+
+        emit Mint(tokenId, to);
     }
 
+    function burn(uint256 tokenId) public isSuperOwner() {
+        require(tokenId == tokenCount, "ERC721: token does not exist");
+
+        address previousOwner = _tokenOwners[tokenId];
+
+        delete _holderTokens[previousOwner];
+
+        delete _tokenOwners[tokenId];
+
+        tokenCount -= 1;
+
+        emit Burn(tokenId);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return tokenCount;
+    }
 }
--- migrations/2_deploy_spartan_council.js
+++ migrations/2_deploy_spartan_council.js
@@ -4,11 +4,10 @@ module.exports = function (deployer, network, accounts) {
   const pDAO = accounts[0];
 
   const owner_1 = accounts[1];
-  const owner_2 = accounts[1];
-  const owner_3 = accounts[1];
+  const owner_2 = accounts[2];
+  const owner_3 = accounts[3];
 
-  deployer.deploy(SpartanCouncil, {
-    name: "Spartan Council",
-    symbol: "SC",
+  deployer.deploy(SpartanCouncil, "Spartan Council", "SC", {
+    from: pDAO,
   });
 };
