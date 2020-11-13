fb9e8377638af365bae2c0a68aa5fdaa8c73178a
AndyTCF <andy.c.f.98@gmail.com>
2020-11-13 13:14:52 +1100

    Implement super owner

--- migrations/2_deploy_spartan_council.js
+++ migrations/2_deploy_spartan_council.js
@@ -1,7 +1,12 @@
 const SpartanCouncil = artifacts.require("SpartanCouncil");
 
 module.exports = function (deployer, network, accounts) {
-  const owners = [accounts[0], accounts[1]];
+  const pDAO = accounts[0];
+
+  const owner_1 = accounts[1];
+  const owner_2 = accounts[1];
+  const owner_3 = accounts[1];
+
   deployer.deploy(SpartanCouncil, {
     name: "Spartan Council",
     symbol: "SC",
